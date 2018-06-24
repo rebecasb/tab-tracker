8428f1612ae763e827865708683cebc485d37d89
rebeca_salazarb <rebeca.salazarb@gmail.com>
2018-06-24 18:46:12 -0400

    Configuración

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Register from '@/components/Register'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
     }
   ]
 })
